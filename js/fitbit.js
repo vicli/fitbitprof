7b3cca367e065fcad86652560047845e95440940
Victoria Li <vli@fitbit.com>
2015-10-22 16:07:15 -0700

    initial commit

---
+++ js/fitbit.js
@@ -0,0 +1,30 @@
+Items = new Mongo.Collection("items");
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+  Template.body.helpers({
+    items: function(){
+      Items.find({});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
