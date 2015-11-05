09b19c308c401e89e0b1bdcffabead813d57dd54
Victoria Li <vli@fitbit.com>
2015-11-05 13:48:52 -0800

    Added dynamic template

--- js/fitbit.js
+++ js/fitbit.js
@@ -1,15 +1,39 @@
 MenuItems = new Mongo.Collection("menuitems");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  if (MenuItems.find({}).count() === 0) {
-       
-  }
+
+  /* SIDE PANEL HELPERS */
   Template.side_panel.helpers({
     items: function(){
       return MenuItems.find({});
     }
   });
+
+  Template.panels.onCreated(function(){
+    currentTab = new ReactiveVar("personal_info");
+  })
+
+  Template.panels.helpers({
+    tab: function() {
+      console.log("Tab", currentTab.get());
+      return currentTab.get();
+    },
+    template: function() {
+      return 'personal_info';
+      // console.log("oh helllo");
+    }
+  });
+
+  Template.side_panel.events({
+    'click .side-panel-contents li': function(event, template) {
+      var newTab = $( event.target ).closest( "li" );
+      currentTab.set(newTab.data("template"));
+    }
+  })
+
+  Template.personal_info.onCreated(function(){
+    mainPanel = Template.instance();
+  })
 }
 
 if (Meteor.isServer) {
